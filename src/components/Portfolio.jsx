329319de7e0a27acf010eee13c3c8c9dd06d9144
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2022-10-16 00:54:35 -0700

    spacing check

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -36,9 +36,9 @@ const Portfolio = () => {
 
 
   return (
-    <div name="Portfolio" className='bg-gradient-to-b from-black to-gray-800 text-white md:h-screen pt-16'>
+    <div name="Portfolio" className='bg-gradient-to-b from-black to-gray-800 text-white md:h-screen pt-16 pb-8'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
+            <div className='pb-8 text-center'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
                 <p className='py-6'>Check out my work right here</p>
             </div>
