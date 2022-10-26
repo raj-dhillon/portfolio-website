2ccf5390adb46efa42abc86140346bc9f4bf815d
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2022-10-25 19:33:53 -0700

    testing new margins

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name='About' className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white pt-16 pb-8'>
+    <div name='About' className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white pt-16 sm:pb-8 pb-16'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
             <div className='pb-8 mt-8 text-center'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
             </div>
 
-            <p className='text-xl mt-20'>
+            <p className='text-xl sm:mt-20'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lectus tortor, interdum at auctor id, blandit et libero. Etiam ultrices orci laoreet ex malesuada, nec finibus nibh semper. Mauris in eros sodales, laoreet est in, auctor risus. Nulla imperdiet quam sed augue vulputate, ut interdum magna convallis. Duis bibendum magna ligula, et mattis lacus convallis in. Nam fermentum hendrerit ex ac convallis. Proin sodales pellentesque erat a gravida. In pellentesque eros vel vehicula imperdiet.
             </p>
             <br></br>
